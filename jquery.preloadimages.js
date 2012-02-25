757fff0b56537d0967a00355985632ef30edace1
Kevin Boudloche <kevin@tentonaxe.com>
2012-02-24 20:59:47 -0600

    Update code to v2.1.0

--- jquery.preloadimages.js
+++ jquery.preloadimages.js
@@ -1,88 +1,93 @@
+;(function($) {
 /*
- * jQuery preloadImages v1.0.0
+ * jQuery preloadImages v2.1.0
  * http://www.tentonaxe.com/
  *
  * Copyright 2012 Kevin Boudloche
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Date: 01/13/2012
+ * Date: 02/24/2012
  */
-(function($) {
-    $.preloadImages = function(imgArr, callback) {
-        if ($.type(imgArr) !== "array" || imgArr.length === 0) {
-            callback();
-            return;
-        }
-        //Keep track of the images that are loaded
-        var imagesLoaded = 0;
+$.preloadImages = function( imgArr, callback ) {
+	var def = $.Deferred(), imagesLoaded = 0, defArr = [];
 
-        function _loadAllImages() {
-            //Create a temp image and load the url
-            var img = new Image();
-            $(img).attr('src', imgArr[imagesLoaded]);
-            if (img.complete || img.readyState === 4) {
-                // image is cached
-                imagesLoaded++;
-                //Check if all images are loaded
-                if (imagesLoaded == imgArr.length) {
-                    //If all images loaded do the callback
-                    callback();
-                } else {
-                    //If not all images are loaded call own function again
-                    _loadAllImages();
-                }
-            } else {
-                $(img).load(function() {
-                    //Increment the images loaded variable
-                    imagesLoaded++;
-                    //Check if all images are loaded
-                    if (imagesLoaded == imgArr.length) {
-                        //If all images loaded do the callback
-                        callback();
-                    } else {
-                        //If not all images are loaded call own function again
-                        _loadAllImages();
-                    }
-                });
-            }
-        }
-        _loadAllImages();
-        return imgArr;
-    };
+	/*
+	 * This function performs a single image preload
+	 */
+	function _preloadImage ( url, callback, fail ) {
+		var img = new Image();
+		img.src = url;
+		if ( img.complete || img.readyState === 4 ) {
+			callback();
+		}
+    	else {
+			$( img ).bind( "error load onreadystatechange", function ( e ) {
+				//clearTimeout(errorTimer);
+				if (e.type === "error") {
+					fail( "Image failed to load. - " + url);
+				}
+				else {
+					callback(url);
+				}
+			});
+		}
 
-    $.fn.preloadImages = function(callback) {
-        var imgArr = (function() {
-            if (this.filter("img").length) {
-                return this.filter("img");
-            }
-            else {
-                return this.find("img");
-            }
-        }).call(this),
-            def = $.Deferred(),
-            counter = imgArr.length - 1;
-        this.done = def.promise().done;
+	}
 
-		// loop through each image, sending each one individually to the
-		// $.preloadImages method
-        imgArr.each(function() {
-            var img = this;
-            $.preloadImages([this.src], function() {
-            	
-            	// if callback is defined, run it with img as context
-                callback && callback.call(img);
-                
-                // decrement the counter, and if it is 0, resolve deferred object
-                if (!counter--) {
-                    def.resolve();
-                }
-            });
-        });
-        
-        // if imgArr was empty, go ahead and resolve the deferred object
-        if (imgArr.length === 0) {
-            def.resolve();
-        }
-        return this;
-    };
-})(jQuery);
+	/*
+	 * If a callback was passed to the plugin, bind it
+	 * to the always callback of the deferred
+	 */
+	if ( $.type( callback ) === "function" ) {
+		def.always( callback );
+	}
+
+	/*
+	 * If an empty array is passed to the plugin, 
+	 * immediately resolve and exit.
+	 */
+	if ( $.type( imgArr ) === "array" && imgArr.length === 0 ) {
+		def.resolve();
+		return def.promise();
+	} 
+
+	/*
+	 * If a url is passed as the first argument,
+	 * preload the url.
+	 */
+	if ( typeof imgArr === "string" ) {
+		_preloadImage( imgArr, def.resolve, def.reject );
+		return def.promise();
+	}
+
+	/*
+	 * One last check to make sure that imgArr is
+	 * defined and is an array
+	 */
+	if ( !imgArr || $.type( imgArr ) !== "array" ) {
+		def.resolve();
+		return def.promise();
+	}
+
+	/*
+	 * If we've gotten this far, the first argument
+	 * is more than likely an array of images. Loop
+	 * through the array and preload each image. When
+	 * done, resolve the deferred object.
+	 */
+	$.each( imgArr, function ( i, url ) {
+
+		// add a new deferred object onto the array at this index
+		defArr[ i ] = $.Deferred();
+
+		// preload the image and resolve the deferred when done
+		_preloadImage( url, defArr[ i ].resolve, defArr[ i ].reject );
+
+	});
+
+	// When all deferreds in defArr are resolved, resolve the overall deferred object.
+	$.when.apply( $, defArr ).then( def.resolve, def.reject );
+		return def.promise();
+	};
+
+})( jQuery );
